1e7608e8fd119d7e1c23e42df5ef4d56955da129
wagnerb3 <wagnerb@udel.edu>
2020-04-16 22:24:30 -0400

    Initial Commit

---
+++ Squirrel.py
@@ -0,0 +1,34 @@
+'''
+Author: wagnerb@udel.edu
+Updated: 2/13/2020
+Description:  Summate a sequence of numbers while skipping certain ones.
+              The total, or corresponding message is then output.
+'''
+
+# Get the filename from stdin
+filename = input()
+
+# Reads individual lines from file into a list
+with open(filename) as i:
+    lines = i.readlines()
+
+# Determine number of lines to read
+repeat = int(lines[0])
+
+# Initialize total
+total = 0
+
+# Loops through lines in file and adds positive numbers to total
+for x in range(repeat):
+    if int(lines[x+1]) == -999:
+        break
+    elif int(lines[x+1]) >= 0:
+        total += int(lines[x+1])
+    else:
+        continue
+
+# Output total or corresponding message
+if total > 0:
+    print (total)
+else:
+    print ("EMPTY")
